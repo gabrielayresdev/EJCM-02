61a00ae9110bc0facc2f9c7f1d46e599882f5416
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2024-01-12 10:23:14 -0300

    feat: feed general styles

--- js/modules/loadFeed.js
+++ js/modules/loadFeed.js
@@ -6,23 +6,41 @@ const posts = [
   },
   {
     author: "HIDEO_KOJIMA",
-    img: "/assets/post_image.png",
-    text: "With Al Pacino san.",
+    img: "/assets/post_image2.png",
+    text: "With Norman.",
   },
   {
     author: "HIDEO_KOJIMA",
-    img: "/assets/post_image.png",
-    text: "With Al Pacino san.",
+    img: "/assets/post_image3.png",
+    text: "Our Keanu visited Kojima Productions",
   },
 ];
 
 const createFeed = (author, img, text) => {
   const container = document.createElement("div");
   container.innerHTML = `
-    <h3>${author}</h3>
-    <img src="${img}" alt="Image posted by ${author}"/>
-    <p>${text}</p>
+    <div class="author">
+        <a href="#" class="profile_pic">
+            <img src="/assets/profile_pic.png" alt="go to author profile" />
+        </a>
+        <h3 class="author">${author}</h3>
+    </div>
+    <div class="content">
+        <div class="image">
+            <img src="${img}" alt="Image posted by ${author}"/>
+        </div>
+        <div class="post_interactions">
+            <span>
+                <img class="button" src="/assets/like.svg" alt="like post"/>
+                <img class="button" src="/assets/comment.svg" alt="comment post" />
+            </span>
+            <img class="button" src="/assets/favorite.svg" alt="favorite post" />
+        </div>
+        <p class="text">${text}</p>
+    </div>
     `;
+
+  container.classList.add("post");
 
   return container;
 };
